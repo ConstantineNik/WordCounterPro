1c1d7b84d9796b7966da40532aba45fce1d14b5d
nikitchenko <kostyanikitchenko@gmail.con>
2021-10-29 20:46:29 +0300

    Initial Commit

---
+++ WorldCounterPro/WorldCounterProApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldCounterProApp.swift
+//  WorldCounterPro
+//
+//  Created by Kostiantyn Nikitchenko on 29.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct WorldCounterProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
